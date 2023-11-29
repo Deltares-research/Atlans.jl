f92632eff9285960e0636f0300c208c34d2bf8c5
basknaake <bas.knaake@gmail.com>
2023-11-29 12:58:03 +0100

    Bugfix carbon_loss on CarbonStore cells

--- src/soilcolumn/split.jl
+++ src/soilcolumn/split.jl
@@ -101,7 +101,7 @@ function cellsplit!(
             cell.α0,
             cell.α,
             NaN,
-            cell.Δm
+            cell.carbon_loss
         )
         upper = CarbonStore(
             upperΔz,
@@ -112,7 +112,7 @@ function cellsplit!(
             cell.α0,
             cell.α,
             NaN,
-            cell.Δm
+            cell.carbon_loss
         )
 
         insert!(column.cells, index, lower)
